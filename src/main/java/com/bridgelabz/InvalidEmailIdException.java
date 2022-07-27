d63ce58aa4b08c17f46b9c729900ee359d1f5017
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-27 15:47:49 +0530

    Added: Refactored the code and added Exceptions

---
+++ src/main/java/com/bridgelabz/InvalidEmailIdException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class InvalidEmailIdException extends Exception {
+        public InvalidEmailIdException(String message) {
+
+            super(message);
+        }
+}
